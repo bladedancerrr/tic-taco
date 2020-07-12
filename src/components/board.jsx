d4e2f097ad65d928576d86e02c90f072e8453e66
Ayesha Ahmed <ayeshaa1@student.unimelb.edu.au>
2020-07-12 13:06:29 +1000

    Created board and square component skeletons.

---
+++ src/components/board.jsx
@@ -0,0 +1,14 @@
+{
+  /*Component that represents the tic-tac-toe board made up of squares.*/
+}
+
+import React, { Component } from "react";
+
+class Board extends Component {
+  state = {};
+  render() {
+    return null;
+  }
+}
+
+export default Board;
---
+++ src/components/square.jsx
@@ -0,0 +1,11 @@
+{
+  /*Component that represents each individual square on the board.*/
+}
+
+import React, { Component } from "react";
+
+const Square = (props) => {
+  return null;
+};
+
+export default Square;
